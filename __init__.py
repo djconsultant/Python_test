40f0b41b40778b7a496285fe2fa017f87086ac57
djconsultant <115322582+djconsultant@users.noreply.github.com>
2022-11-01 11:07:44 -0700

    update init file

--- __init__.py
+++ __init__.py
@@ -1,2 +1,7 @@
 from flask import Flask
 
+# The following create a Flask App with __name__ refering to "PYTHON_TEST"
+# or you can also write this as:  app = Flask("PYTHON_TEST")
+app = Flask(__name__)
+
+from app import routes
